71f99bd6ba8b5753b47ec45991aba2c185f77728
Alex Joshua Chirwa <153588378+AlexChirwa@users.noreply.github.com>
2024-06-29 17:03:10 +0800

    Update CountingDown.java

--- CountingDown.java
+++ CountingDown.java
@@ -1,19 +1,28 @@
-
+/*
+Date: 6.29.2024
+Author: Chirwa Alex Joshua
+*/
 import java.util.Scanner;
 
 public class CountingDown {
 	public static void main(String[] args) {
+		// prompt user to enter a positive number
 		Scanner out = new Scanner(System.in);
 		System.out.print("Enter a positive number: ");
 		
 		int num = out.nextInt();
-		
+
 		for(int x = num;x >= 0;x--) {
 			System.out.println(x);
-			sleep(1000);
+			sleep(1000); // this ensures the counting of numbers one at a time in the console 
 		}
-		out.close();
+		out.close(); // close the Scanner 
 	}
+
+	/*
+           This method pauses the execution of the program for the specified number of milliseconds. 
+	   The Thread.sleep method is used to introduce the delay.
+	*/
 	private static void sleep(int milliseconds) {
 		try {
 			Thread.sleep(milliseconds);
