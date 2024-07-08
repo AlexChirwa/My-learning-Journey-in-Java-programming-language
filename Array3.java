5ec89e15b68480a4122c113a2f0b650be3f74cfb
Alex Joshua Chirwa <153588378+AlexChirwa@users.noreply.github.com>
2024-07-08 17:53:32 +0800

    Add files via upload

---
+++ Array3.java
@@ -0,0 +1,16 @@
+/* Date: 7.8.2024
+ * Author: Chirwa Alex Joshua
+ * Change an Array element 
+ */
+
+public class Array3{
+	public static void main(String[] args) {
+		
+		String[] cars = {"Volvo", "Bens", "BMW"};
+		cars[0] = "Opel";
+		
+		System.out.print(cars[0]); // Output: opel instead of Volvo
+		
+	}
+}
+
---
+++ Arrays4.java
@@ -0,0 +1,14 @@
+/* Date: 7.8.2024
+ * Author: Chirwa Alex Joshua
+ * Array length 
+ */
+
+public class Arrays4{
+	public static void main(String[] args) {
+		// we use the properties of length to get the length of an array
+		
+		// Variable initialization
+		String[] names = {"Ben", "Josh", "Alex"};
+		System.out.print(names.length); // output: 3
+	}
+}
