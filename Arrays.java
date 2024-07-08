94e216e93968bd2ffe5455073f63816f01abca69
Alex Joshua Chirwa <153588378+AlexChirwa@users.noreply.github.com>
2024-07-08 17:53:16 +0800

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,29 @@
+/* Date: 7.8.2024
+ * Author: Chirwa Alex Joshua
+ * Arrays  
+ */
+
+
+public class Arrays{
+	public static void main(String[] args) {
+		/* Storing an Array we add brackets to the variable type followed by the variable
+		 * we assign the elements to the variable in curly braces 
+		 * Example
+		 * variable name = {};
+		 * Variable type []
+		 *  
+		 */
+		
+		// Variable initialization
+		String[] cars = {"Volvo", "Bens", "BMW"};
+		
+		// Output
+		System.out.println(cars[0]); // Output: Volvo
+		
+		
+		//integer arrays
+		int[] numbers = {1, 2, 3, 4, 5};
+		
+		System.out.println(numbers[0]);
+	}
+}
---
+++ Arrays2.java
@@ -0,0 +1,18 @@
+/* Date: 7.8.2024
+ * Author: Chirwa Alex Joshua
+ *  Access an Arrays  
+ */
+
+
+public class Arrays2{
+	public static void main(String[] args) {
+		// access an array
+		
+		// Variable initialization
+		String[] cars = {"Volvo", "Bens", "BMW"};
+		
+		// output
+		System.out.print(cars[1]); // Output Volvo
+		
+	}
+}
